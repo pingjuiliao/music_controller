7d49b63fbbe67e783208a39135378ecf86a2be46
pingjui <pingjuiliao@gmail.com>
2024-07-14 23:48:58 -0700

    change to functional

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 import Room from "./Room";
 import RoomJoinPage from "./RoomJoinPage";
@@ -9,67 +9,59 @@ import {
   Routes,
   Route,
   Link,
-  Redirect,
+  Navigate
 } from "react-router-dom";
 
-export default class HomePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      roomCode: null,
-    }
-  }
-
-  async componentDidMount() {
+export default function HomePage() {
+  let [roomCode, setRoomCode] = useState(null);
+  useEffect(() => {
     fetch('/api/user-in-room')
       .then((response) => response.json())
       .then((data) => {
-        this.setState({ roomCode: data.code });
+        setRoomCode(data.code);
       });
-  }
+  });
 
-  renderHomePage() {
+  function renderHomePage() {
     return (
       <Grid container spacing={3}>
-        <Grid item xs={12} align="center">
-          <Typography variant="h3" compact="h3">
-            House Party
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <ButtonGroup disableElevation variant="contained" color="primary">
-            <Button color="primary" to="/join" component={Link}>
-              Join a Room
-            </Button>
-            <Button color="success" to="/create" component={Link}>
-              Create a Room
-            </Button>
-          </ButtonGroup>
-        </Grid>
+      <Grid item xs={12} align="center">
+      <Typography variant="h3" compact="h3">
+      House Party
+      </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+      <ButtonGroup disableElevation variant="contained" color="primary">
+      <Button color="primary" to="/join" component={Link}>
+      Join a Room
+      </Button>
+      <Button color="success" to="/create" component={Link}>
+      Create a Room
+      </Button>
+      </ButtonGroup>
+      </Grid>
       </Grid>
     );
   }
 
-  homeConditionalRoute() {
-    if (this.state.roomCode) {
+  function homeConditionalRoute() {
+    if (roomCode) {
       return (
-        <Redirect to={`/room/${this.state.roomCode}`} />
+        <Navigate to={`/room/${roomCode}`} />
       );
     }
-    return this.renderHomePage();
+    return renderHomePage();
   }
 
 
-  render() {
-    return (
-      <Router>
-        <Routes>
-          <Route exact path="/" element={this.homeConditionalRoute()} />
-          <Route path="/join" element={<RoomJoinPage />} />
-          <Route path="/create" element={<CreateRoomPage />} />
-          <Route path="/room/:roomCode" element={<Room />} />
-        </Routes>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+    <Routes>
+    <Route exact path="/" element={homeConditionalRoute()} />
+    <Route path="/join" element={<RoomJoinPage />} />
+    <Route path="/create" element={<CreateRoomPage />} />
+    <Route path="/room/:roomCode" element={<Room />} />
+    </Routes>
+    </Router>
+  );
 }
